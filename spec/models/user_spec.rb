9f5f6a167c05d98a3a4e945e1f8d1f75b510058d
sbog8 <shareni.ortega@udem.edu>
2019-02-05 12:13:39 -0600

    Invalid email address test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
 
   describe "#new" do
     it "should be valid" do
-      assert user.valid?
+      expect(user.valid?).to be_truthy
     end
 
     it "should return false if name's blank" do
@@ -26,6 +26,24 @@ RSpec.describe User, type: :model do
     it "should return false if email's too long (>255)" do
       user.email = "a" * 244 + "@hotmail.com"
       expect(user.valid?).to be_falsey
+    end
+
+    it "should be true with a valid address" do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        user.email = valid_address
+        assert user.valid?, "#{valid_address.inspect} should be valid"
+      end
+    end
+
+    it "should be false with invalid address" do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        user.email = invalid_address
+        expect(user.valid?).to be_falsey, "#{invalid_address.inspect} should be invalid"
+      end
     end
   end
 end
