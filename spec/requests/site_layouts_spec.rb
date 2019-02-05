035a4f9be289635c5c235ed4bfddbade6db5e787
sbog8 <shareni.ortega@udem.edu>
2019-02-05 09:57:55 -0600

    Test for layout links

--- spec/requests/site_layouts_spec.rb
+++ spec/requests/site_layouts_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
+require 'rails-controller-testing'
 
 RSpec.describe "SiteLayouts", type: :request do
-  describe "GET /site_layouts" do
-    it "works! (now write some real specs)" do
-      get site_layouts_path
-      expect(response).to have_http_status(200)
+  describe "layout links" do
+    it "should work in home page" do
+      get "/"
+      assert_template 'static_pages/home'
+      assert_select "a[href=?]", '/', count: 2
+      assert_select "a[href=?]", '/about'
+      assert_select "a[href=?]", '/help'
+      assert_select "a[href=?]", '/contact'
     end
   end
 end
