06b30785b6910792ca673700662b7e06022ba003
sbog8 <shareni.ortega@udem.edu>
2019-02-15 10:13:38 -0600

    Dependant of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
     attr_accessor :remember_token, :activation_token, :reset_token
     before_create :create_activation_digest
-    has_many :microposts
+    has_many :microposts, dependent: :destroy
 
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
