300583e8c99cae0e323c950df46be6da901ba084
sbog8 <shareni.ortega@udem.edu>
2019-02-15 09:56:53 -0600

    A user has_many microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
     attr_accessor :remember_token, :activation_token, :reset_token
     before_create :create_activation_digest
+    has_many :microposts
 
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
