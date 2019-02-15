b8586287b67731733a1072ac9ebca6fdbd1e79cf
sbog8 <shareni.ortega@udem.edu>
2019-02-15 15:13:25 -0600

    Add user followers and following

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,15 +81,16 @@ class User < ApplicationRecord
     end
 
     def feed
-        Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships
+                         WHERE  follower_id = :user_id"
+        Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id",
+                         user_id: id)
     end
 
-    # Follows a user.
     def follow(other_user)
         following << other_user
     end
 
-    # Unfollows a user.
     def unfollow(other_user)
         following.delete(other_user)
     end
